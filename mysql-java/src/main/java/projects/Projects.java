039ef25d4a48f5951fd0d8da382caa4d2f1cab4c
THUMP3R WALKER <jamieleewalker814@gmail.com>
2023-06-17 14:53:02 -0600

    first commit

---
+++ mysql-java/src/main/java/projects/Projects.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package projects;
+
+
+import projects.dao.DbConnection;
+
+/**
+ * @author jamie
+ *
+ */
+public class Projects {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		DbConnection.getConnection();
+	}
+
+}
